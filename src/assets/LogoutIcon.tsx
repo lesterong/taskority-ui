5ad308953feee0f5349faf77ca8b18998df15e50
Lester <lesterong776@gmail.com>
2022-01-23 19:40:22 +0800

    Fix alignment issue

--- src/assets/LogoutIcon.tsx
+++ src/assets/LogoutIcon.tsx
@@ -10,7 +10,6 @@ export const LogoutIcon = () => {
       strokeWidth='1.5'
       strokeLinecap='round'
       strokeLinejoin='round'
-      className='mx-auto'
     >
       <path d='M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4'></path>
       <polyline points='16 17 21 12 16 7'></polyline>
